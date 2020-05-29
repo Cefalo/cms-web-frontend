6886d4523696f81fda25ca8398870d6d74d2cce4
A K M Rezaul Karim <tito@cefalo.com>
2020-05-29 23:44:15 +0600

    FE #1 login page using material-ui

---
+++ src/components/MainComponent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+
+/* Custom components */
+import SignIn from './SignIn';
+
+
+class Main extends Component {
+
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <Switch location={this.props.location}>
+          <Route path='/signin' component={SignIn} />
+          <Redirect to="/signin" />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default withRouter(Main);
